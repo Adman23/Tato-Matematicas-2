c9153118aa3b8d5496f245239d26828d916cc696
claraagrcia <ruoda.cg@gmail.com>
2025-10-27 18:19:01 +0100

    Interfaz vincular perfiles terminada

--- frontend/src/pages/admin/LinkProfiles.tsx
+++ frontend/src/pages/admin/LinkProfiles.tsx
@@ -2,6 +2,9 @@ import {
     IonContent,
     IonSpinner,
     IonPage,
+    IonList,
+    IonTitle,
+    IonButton
 
 } from '@ionic/react';
 
@@ -9,7 +12,8 @@ import './LinkProfiles.css';
 import { useState } from 'react';
 import SimpleHeaderAdmin from './components/SimpleHeaderAdmin';
 import { useAuth } from '../../contexts/AuthContext';
-import TeacherSelect from './components/TeacherSelect';
+import ClassSelect from './components/ClassSelect';
+import UserItem from './components/UserItem';
 
 export default function LinkProfiles() {
 
@@ -34,21 +38,128 @@ export default function LinkProfiles() {
 
     const [selectedTeacher, setSelectedTeacher] = useState<string>("");
 
-    const teachers = [
-        { id: '1', username: 'profesor1' },
-        { id: '2', username: 'profesor2' },
-        { id: '3', username: 'profesor3' },
+    const classes = [
+        { id: '1', name: 'Clase A' },
+        { id: '2', name: 'Clase B' },
+        { id: '3', name: 'Clase C' },
     ]
+
+    const students = [
+        {
+            avatar: "https://ionicframework.com/docs/img/demos/avatar.svg",
+            alias: "Alias",
+            classes: ['Clase B']
+        },
+        {
+            avatar: "https://ionicframework.com/docs/img/demos/avatar.svg",
+            alias: "Alias",
+            classes: ['Clase A']
+        },
+        {
+            avatar: "https://ionicframework.com/docs/img/demos/avatar.svg",
+            alias: "Alias",
+            classes: ['Clase A']
+        },
+        {
+            avatar: "https://ionicframework.com/docs/img/demos/avatar.svg",
+            alias: "Alias",
+            classes: ['-']
+        },
+        // agrega más estudiantes o tráelos del backend
+    ];
+
+    const teachers = [
+        {
+            avatar: "https://ionicframework.com/docs/img/demos/avatar.svg",
+            alias: "Alias",
+            classes: ['Clase B']
+        },
+        {
+            avatar: "https://ionicframework.com/docs/img/demos/avatar.svg",
+            alias: "Alias",
+            classes: ['Clase A', 'Clase B']
+        },
+        {
+            avatar: "https://ionicframework.com/docs/img/demos/avatar.svg",
+            alias: "Alias",
+            classes: ['Clase A']
+        },
+        {
+            avatar: "https://ionicframework.com/docs/img/demos/avatar.svg",
+            alias: "Alias",
+            classes: ['-']
+        },
+        // agrega más estudiantes o tráelos del backend
+    ];
 
     return (
         <IonPage>
             <SimpleHeaderAdmin adminName="Admin" />
             <IonContent className="ion-padding">
-                <TeacherSelect
-                    teachers={teachers}
+                <ClassSelect
+                    classes={classes}
                     value={selectedTeacher}
                     onChange={setSelectedTeacher}
+                    label='Clase:'
+                    max_width='30%'
+                    placeholder_text='Selecciona una clase'
                 />
+
+                <div className='LinkProfiles-tables'>
+                    <div className='LinkProfiles-table'>
+                        <IonTitle className='LinkProfiles-title'>Alumnos</IonTitle>
+                        <div className='LinkProfiles-items'>
+
+                            <IonList>
+                                {students.map(student => (
+                                    <UserItem
+                                        avatar={student.avatar}
+                                        alias={student.alias}
+                                        classes={student.classes}
+                                    />
+                                ))}
+                            </IonList>
+
+                        </div>
+                    </div>
+
+                    <div className='LinkProfiles-table'>
+                        <IonTitle className='LinkProfiles-title'>Profesores</IonTitle>
+                        <div className='LinkProfiles-items'>
+
+                            <IonList>
+                                {teachers.map(teacher => (
+                                    <UserItem
+                                        avatar={teacher.avatar}
+                                        alias={teacher.alias}
+                                        classes={teacher.classes}
+                                    />
+                                ))}
+                            </IonList>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div className='LinkProfiles-buttons'>
+                    <IonButton
+                        expand="block"
+                        type="submit"
+                        className='LinkProfiles-button'
+                    //onClick={() => history.push('/admin-teacher-management')}
+                    >
+                        Asignar
+                    </IonButton>
+
+                    <IonButton
+                        expand="block"
+                        type="submit"
+                        className='LinkProfiles-button'
+                    //onClick={() => history.push('/admin-teacher-management')}
+                    >
+                        Cancelar
+                    </IonButton>
+                </div>
             </IonContent>
         </IonPage>
 
