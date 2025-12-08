28541578a153fd8b919717e0f1c0a98c31c987e6
Manuel Enríquez Ledesma <126668772+manuenrle@users.noreply.github.com>
2025-12-08 17:18:24 +0100

    Modificado y adaptado frontend/src/lib/api.ts para soportar password_length en el frontend

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -139,6 +139,7 @@ export interface User {
   role: Role;
   photo_url?: string;
   password_type?: PasswordType;
+  password_length?: number;
   group_id?: string;
   group_alias?: string;
 }
@@ -150,6 +151,7 @@ export interface User {
 export interface UserData {
   username: string;
   password_type: PasswordType;
+  password_length: number;
   user_profile: any;
   game_configurations: any;
   reinforcement_messages: any;
@@ -169,6 +171,7 @@ export interface UserComplete {
   photo_url?: string;
   group_id?: string;
   password_type: PasswordType;
+  password_length: number;
   group_alias?: string;
   user_profile: any;
   game_configurations: any;
@@ -192,6 +195,7 @@ export interface AuthResponse {
 export interface RegisterData {
   username: string;
   password: string;
+  password_length: number;
   role: Role;
   photo_url?: string;
 }
@@ -239,6 +243,7 @@ export interface UserUpdatePayload {
   password?: string;
   photo_url?: string;
   password_type?: PasswordType;
+  password_length?: number;
 }
 
 
